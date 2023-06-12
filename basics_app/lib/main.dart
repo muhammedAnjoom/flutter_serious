f98d0339642567b5cd5852f983eaba9cb690b402
muhammedAnjoom <anjoompa42003@gmail.com>
2023-06-12 20:34:05 +0530

    flutter start basics screen

---
+++ basics_app/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Text("hello world"),
+      ),
+    );
+  }
+}
