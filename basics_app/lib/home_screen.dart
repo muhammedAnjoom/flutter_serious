b64a4eb446ae858a2df439ca73802f0a08d9baa4
muhammedAnjoom <anjoompa42003@gmail.com>
2023-06-19 13:49:49 +0530

    validation and alert messages

--- basics_app/lib/home_screen.dart
+++ basics_app/lib/home_screen.dart
@@ -3,69 +3,132 @@ import 'package:basics_app/screen_two.dart';
 import 'package:basics_app/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({
     super.key,
   });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final _textController = TextEditingController();
+
   final _textConifromController = TextEditingController();
+
+  bool _validate = true;
+  final _fromKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    // _textConifromController.dispose();
+    // _textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                  ),
-                  child: TextFormField(
-                    controller: _textController,
-                    decoration: const InputDecoration(
-                      hintText: "userName",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                    ),
-                  ),
+      child: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Form(
+          key: _fromKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                  ),
-                  child: TextFormField(
-                    controller: _textConifromController,
-                    decoration: const InputDecoration(
-                      hintText: "conform",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    saveDataToStorge(context);
+                child: TextFormField(
+                  controller: _textController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'error';
+                    } else {
+                      return null;
+                    }
                   },
-                  child: const Text(
-                    "login",
+                  decoration: const InputDecoration(
+                    hintText: "userName",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
                   ),
-                )
-              ],
-            ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: TextFormField(
+                  obscureText: true,
+                  controller: _textConifromController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'error';
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: const InputDecoration(
+                    // errorText: !_validate ? 'can\'t be empty' : null,
+                    hintText: "conform",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Visibility(
+                      visible: !_validate,
+                      child: const Text(
+                        "Invalied username and password",
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        _fromKey.currentState!.validate()
+                            ? saveDataToStorge(context)
+                            : null;
+                        // setState(() {
+                        //   _textController.text.isEmpty?_validate =true:_validate=false;
+                        //    _textConifromController.text.isEmpty?_validate =true:_validate=false;
+                        // });
+                      },
+                      child: const Text(
+                        "login",
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    ));
   }
 
   Future<void> saveDataToStorge(context) async {
     // check data
-    if (_textController.text == _textConifromController.text) {
+    final _username = _textController.text;
+    final _confirm = _textConifromController.text;
+    if (_username == _confirm) {
       // save value using sharedPreferences
       await sharedPreferences.setString('saved_value', _textController.text);
       Navigator.of(context).pushAndRemoveUntil(
@@ -73,8 +136,37 @@ class HomeScreen extends StatelessWidget {
         (route) => false,
       );
     } else {
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (ctx) => const SplahScreen()));
+      final _errorMessage = "Invalied username and password";
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          behavior: SnackBarBehavior.floating,
+          margin: const EdgeInsets.all(10),
+          backgroundColor: Colors.grey,
+          content: Text(_errorMessage),
+        ),
+      );
+
+      // showDialog(
+      //   context: context,
+      //   builder: (ctx) {
+      //     return AlertDialog(
+      //       title: const Text("Error"),
+      //       content: Text(_errorMessage),
+      //       actions: [
+      //         TextButton(
+      //           onPressed: () {
+      //             Navigator.of(ctx).pop();
+      //           },
+      //           child: const Text("close"),
+      //         )
+      //       ],
+      //     );
+      //   },
+      // );
+      setState(() {
+        _validate = false;
+      });
     }
   }
 }
