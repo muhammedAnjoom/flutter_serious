252fb2ef987e2de9093e9a4a634043ee6fbb35f1
muhammedAnjoom <anjoompa42003@gmail.com>
2023-07-26 22:04:24 +0530

    user deatils add

--- copy_ui/lib/screen/orderDetails/order_detalis_screen..dart
+++ copy_ui/lib/screen/orderDetails/order_detalis_screen..dart
@@ -1,6 +1,7 @@
 import 'package:copy_ui/screen/orderDetails/widget/amount_continer.dart';
 import 'package:copy_ui/screen/orderDetails/widget/product_detalis_continer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -46,14 +47,57 @@ class OrderDetailsScreen extends StatelessWidget {
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text("CUSTOMER DETAILS",style: TextStyle(
-                          color: Colors.black38
-                        ),),
+                        Text(
+                          "CUSTOMER DETAILS",
+                          style: TextStyle(color: Colors.black38),
+                        ),
                         Spacer(),
                         TextButton.icon(
-                            onPressed: () {},
-                            icon: Icon(Icons.share_outlined,size: 20,),
-                            label: Text("SHARE"))
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.share_outlined,
+                            size: 20,
+                          ),
+                          label: Text("SHARE"),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 10,),
+                    Row(
+                      children: [
+                        Column(
+                          // mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "Deepa",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "+91-7829000484",
+                              style: TextStyle(
+                                  fontSize: 16, color: Colors.black87),
+                            ),
+                          ],
+                        ),
+                        const Spacer(),
+                        SvgPicture.asset(
+                          "assets/call1.svg",
+                          width: 30,
+                          height: 30,
+                          color: Colors.blue,
+                        ),
+                        const SizedBox(width: 10,),
+                        SvgPicture.asset(
+                          "assets/whatsapp.svg",
+                          width: 40,
+                          height: 40,
+                          color: Colors.green,
+                        )
                       ],
                     )
                   ],
