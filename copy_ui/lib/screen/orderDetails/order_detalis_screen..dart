1c44de2d254c32aceb63af7e4e63e0d93fc57b44
muhammedAnjoom <anjoompa42003@gmail.com>
2023-07-31 21:47:40 +0530

    order page complete

--- copy_ui/lib/screen/orderDetails/order_detalis_screen..dart
+++ copy_ui/lib/screen/orderDetails/order_detalis_screen..dart
@@ -27,7 +27,7 @@ class OrderDetailsScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(15),
-          child: Column(
+          child: ListView(
             children: [
               const DateDetailsContainer(),
               const Divider(
@@ -43,6 +43,7 @@ class OrderDetailsScreen extends StatelessWidget {
               ),
               Container(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
@@ -62,7 +63,9 @@ class OrderDetailsScreen extends StatelessWidget {
                         )
                       ],
                     ),
-                    const SizedBox(height: 10,),
+                    const SizedBox(
+                      height: 10,
+                    ),
                     Row(
                       children: [
                         Column(
@@ -91,7 +94,9 @@ class OrderDetailsScreen extends StatelessWidget {
                           height: 30,
                           color: Colors.blue,
                         ),
-                        const SizedBox(width: 10,),
+                        const SizedBox(
+                          width: 10,
+                        ),
                         SvgPicture.asset(
                           "assets/whatsapp.svg",
                           width: 40,
@@ -99,8 +104,189 @@ class OrderDetailsScreen extends StatelessWidget {
                           color: Colors.green,
                         )
                       ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Addres",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "D 1101 Chartered Baverly\nHills,Subramanyapura P.O",
+                          style: TextStyle(fontSize: 16, letterSpacing: 0.4),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "City",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              "Bangalore",
+                              style:
+                                  TextStyle(fontSize: 16, letterSpacing: 0.4),
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          width: 100,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "Pincode",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              "560061",
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "Payment",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              "Online",
+                              style:
+                                  TextStyle(fontSize: 16, letterSpacing: 0.3),
+                            )
+                          ],
+                        ),
+                        const Spacer(),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 3, horizontal: 8),
+                          decoration: BoxDecoration(
+                              color: Colors.green[100],
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(3))),
+                          child: Text(
+                            "PAID",
+                            style: TextStyle(
+                                letterSpacing: 0.3,
+                                color: Colors.greenAccent[700],
+                                fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
                     )
                   ],
+                ),
+              ),
+              const Divider(
+                thickness: 1,
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "ADDITIONAL INFORMATION",
+                      style: TextStyle(color: Colors.black38),
+                    ),
+                    SizedBox(height: 12,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "State",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        Text(
+                          "Karnataka",
+                          style: TextStyle(fontSize: 16, letterSpacing: 0.3),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 12,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Email",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        Text(
+                          "greeniceaqua@gmail.com",
+                          style: TextStyle(fontSize: 16, letterSpacing: 0.3),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30,),
+              GestureDetector(
+                onTap: (){},
+                child: Container(
+                  // padding: EdgeInsets.symmetric(vertical: 20),
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    border: Border.all(
+                      color: Colors.blue,width: 1.4
+                    )
+                  ),
+                  child: Center(child: Text("Share receipt",style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.blue[900],
+                    fontWeight: FontWeight.w500
+                  ),)),
                 ),
               )
             ],
