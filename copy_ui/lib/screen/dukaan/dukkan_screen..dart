f2a840bb426c8c9f813aa0c88927205a2612c743
muhammedAnjoom <anjoompa42003@gmail.com>
2023-08-02 06:41:37 +0530

    card text item added

--- copy_ui/lib/screen/dukaan/dukkan_screen..dart
+++ copy_ui/lib/screen/dukaan/dukkan_screen..dart
@@ -29,13 +29,20 @@ class DukkanScreen extends StatelessWidget {
                               Row(
                                 // mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_back,color: Colors.white,)),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        Icons.arrow_back,
+                                        color: Colors.white,
+                                      )),
                                   const SizedBox(width: 90),
-                                  const Text("Dukkan Premium",style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.white
-                                  ),)
+                                  const Text(
+                                    "Dukkan Premium",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.white),
+                                  )
                                 ],
                               )
                             ],
@@ -52,10 +59,43 @@ class DukkanScreen extends StatelessWidget {
                   Align(
                     alignment: Alignment.center,
                     child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 15,vertical: 8),
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 8),
                       height: 180,
-                      // width: 100,
-                      color: Colors.red,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        // boxShadow: [
+                        //   BoxShadow(blurRadius: 1.5, color: Colors.black26)
+                        // ],
+                      ),
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Get Dukaan Premium for just",
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.w400),
+                          ),
+                          Text(
+                            "4,999/year",
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.w400),
+                          ),
+                          Text(
+                            "All the advanced features for scaling your",
+                            style:
+                                TextStyle(fontSize: 16, color: Colors.black45),
+                          ),
+                          Text(
+                            "business",
+                            style:
+                                TextStyle(fontSize: 16, color: Colors.black45),
+                          )
+                        ],
+                      ),
                     ),
                   )
                 ],
